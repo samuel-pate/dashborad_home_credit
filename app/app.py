3cf8eaa32a6cf8e4844c472916ca8d1296bd1377
Samuel PATE <samuel.pate@ac-reims.fr>
2021-01-12 16:25:17 +0100

    Modification du modéle (AUCPR et LabelEncoding) et dashoard

--- app/app.py
+++ app/app.py
@@ -26,7 +26,7 @@ def interface():
 @app.route("/score/<id>", methods=["GET"])
 def score(id):
     try :
-        resp =  clf.predict_proba(np.array(X.loc[int(id)]).reshape(1, -1))[0,1]
+        resp =  clf.predict_proba(np.array(X.loc[int(id)]).reshape(1, -1), num_iteration=clf.best_iteration_)[0,1]
     except KeyError:
         resp = f"Il n'y a pas de client dont l'identifiant est {id}"
     return jsonify(resp)
@@ -35,7 +35,7 @@ def score(id):
 def return_score():
     id = request.args["id"]
     try :
-        resp =  clf.predict_proba(np.array(X.loc[int(id)]).reshape(1, -1))[0,1]
+            resp =  clf.predict_proba(np.array(X.loc[int(id)]).reshape(1, -1), num_iteration=clf.best_iteration_)[0,1]
     except KeyError:
         resp = f"Il n'y a pas de client dont l'identifiant est {id}"
     return jsonify(resp)
@@ -44,10 +44,10 @@ def return_score():
 def display():
     id = request.args["id"]
     try :
-        prob =  clf.predict_proba(np.array(X.loc[int(id)]).reshape(1, -1))[0,1]
+        prob =  clf.predict_proba(np.array(X.loc[int(id)]).reshape(1, -1), num_iteration=clf.best_iteration_)[0,1]
         resp = f"La probabilité de défaut de paiment du client {id} est {prob}"
     except KeyError:
         resp = f"Il n'y a pas de client dont l'identifiant est {id}"
     return render_template("display.html", response = resp )
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
